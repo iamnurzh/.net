5eecbc3f0793d06223b8c4a767e15a75984ef9ab
Nurzhan Nauryzbayev <60682212+iamnurzh@users.noreply.github.com>
2020-04-18 11:44:19 +0600

    Add files via upload

---
+++ MoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using practice4_net.Models.Movies;
+
+
+namespace practice4_net.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
